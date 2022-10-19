ffbb3d680cfca8a93228509604d5f8d6458ca83b
Tim-2112-D <tim.daffner@tum.de>
2022-10-19 16:39:12 +0200

    main.py

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import pygame
 
 pygame.init()
 
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-
-dis_width = 600
-dis_height = 400
+dis_width = 800
+dis_height = 600
 
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake')
