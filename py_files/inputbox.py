b06e006f4ad7ae44fdb09b4cfee4fec84add63fb
Tim <tim.daffner@tum.de>
2023-01-22 17:39:52 +0100

    source

--- py_files/inputbox.py
+++ py_files/inputbox.py
@@ -1,5 +1,6 @@
-### thanks to skrx on stackoverflow 
-### https://stackoverflow.com/questions/46390231/how-can-i-create-a-text-input-box-with-pygame/46390412#46390412
+### thanks to skrx on stackoverflow
+### https://stackoverflow.com/questions/46390231/
+### how-can-i-create-a-text-input-box-with-pygame/46390412#46390412
 
 import pygame
 
